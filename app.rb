86863defa385c6e07c07c6376268427d7f1f4028
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-18 20:28:05 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,24 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do 
+    #accepts name and renders the name backwards
+  end
 
+  get '/square/:number' do
+    #accepts one number and returns the square NOTE: input will be a string use to_i
+  end
+
+  get '/say/:number/:phrase' do
+    #accepts a number and prhase and returns phrase in a string the number of times given 
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    #returns a string containing all five words
+  end
+
+  get '/:operation/:number1/:number2' do
+    #performs operation (add, sub, mul, divide) on two numbers 
+  end
+  
 end
