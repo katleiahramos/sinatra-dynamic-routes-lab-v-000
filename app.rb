91898ad3ab69b4ecaa299a554693ab43efe8125f
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-18 20:37:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
     num = params[:number].to_i
     phrase = params[:phrase]
     output = ""
-    
+
     num.times do |phrase
       binding.pry|
       output << phrase
